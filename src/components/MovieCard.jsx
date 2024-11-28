212baffd3c8f38281104d26f8bbb421d68c47a9d
deca-francesco <francescodecarlo70@gmail.com>
2024-11-28 18:28:11 +0100

    FIX: Flag component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,22 @@
-import getFlag from "./Flag"
+import getFlag from "./Flags"
 
 export default function MovieCard({ data, index }) {
-
 
 
     return (
         <li>
             <h3>{data.title ? data.title : data.name}</h3>
             <p>{data.original_title ? data.original_title : data.original_name}</p>
-            <div><span className="me-2">{getFlag(data.original_language)}</span>{data.original_language}</div>
+            <div>
+                <span className="me-2">
+                    {getFlag(data.original_language) === 'unknown' ? (
+                        `${data.original_language} nazionalità non trovata`
+                    ) : (
+                        <img src={`https://flagcdn.com/w20/${getFlag(data.original_language)}.png`} />)
+                    }
+                </span>
+                {data.original_language}
+            </div>
             <p>{data.vote_average}</p>
         </li>
     )
